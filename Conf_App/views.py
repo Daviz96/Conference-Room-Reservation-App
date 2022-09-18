2ac6c1a5c9daab614aaa2b095f71bb47cbb70a72
Dablio <dawidwrzesinski96@gmail.com>
2022-09-18 20:15:51 +0200

    add new room form

--- Conf_App/views.py
+++ Conf_App/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
 
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        return render(request, template_name='base_template.html')
+
+
+class AddRoom(View):
+    def get(self, request):
+        return render(request, template_name='new_room.html')
+
+    def post(self, request):
+        # <other view logic>
+        return HttpResponse('result')
+
+
--- Conference_Room_App/urls.py
+++ Conference_Room_App/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import Conf_App.views as view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', view.Home.as_view(), name='Home'),
+    path('room/new/', view.AddRoom.as_view(), name='AddRoom')
 ]
